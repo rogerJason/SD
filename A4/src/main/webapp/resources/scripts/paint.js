b47db1917e3359fd29e9aae904fbeababe436153
cipribudusan <cipribudusan@yahoo.com>
2015-05-10 20:40:22 +0300

    [A4] [ADDED] Eraser Tool

--- A4/src/main/webapp/resources/scripts/paint.js
+++ A4/src/main/webapp/resources/scripts/paint.js
@@ -66,6 +66,9 @@
     tmp_canvas.addEventListener('mouseup', function () {
         tmp_canvas.removeEventListener('mousemove', onPaint, false);
 
+        // for erasing, else after erasing won't draw anything
+        ctx.globalCompositeOperation = 'source-over';
+
         // Writing down to real canvas now
         ctx.drawImage(tmp_canvas, 0, 0);
         // Clearing tmp canvas
@@ -83,8 +86,6 @@
         if (ppts.length < 3) {
             var b = ppts[0];
             tmp_ctx.beginPath();
-            //ctx.moveTo(b.x, b.y);
-            //ctx.lineTo(b.x+50, b.y+50);
             tmp_ctx.arc(b.x, b.y, tmp_ctx.lineWidth / 2, 0, Math.PI * 2, !0);
             tmp_ctx.fill();
             tmp_ctx.closePath();
@@ -187,6 +188,49 @@
         tmp_ctx.stroke();
     };
 
+    var onErase = function () {
+
+        // Saving all the points in an array
+        ppts.push({x: mouse.x, y: mouse.y});
+
+        /* Basically, when using this operation, anything that we draw on the canvas, 
+         makes those portions transparent and you can only see the portions on the canvas 
+         that do not overlap with the drawing done after setting this operation */
+        ctx.globalCompositeOperation = 'destination-out';
+        ctx.fillStyle = 'rgba(0,0,0,1)';
+        ctx.strokeStyle = 'rgba(0,0,0,1)';
+        ctx.lineWidth = tmp_ctx.lineWidth;
+
+        if (ppts.length < 3) {
+            var b = ppts[0];
+            ctx.beginPath();
+            ctx.arc(b.x, b.y, ctx.lineWidth / 2, 0, Math.PI * 2, !0);
+            ctx.fill();
+            ctx.closePath();
+
+            return;
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(ppts[0].x, ppts[0].y);
+
+        for (var i = 1; i < ppts.length - 2; i++) {
+            var c = (ppts[i].x + ppts[i + 1].x) / 2;
+            var d = (ppts[i].y + ppts[i + 1].y) / 2;
+
+            ctx.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
+        }
+
+        // For the last 2 points
+        ctx.quadraticCurveTo(
+                ppts[i].x,
+                ppts[i].y,
+                ppts[i + 1].x,
+                ppts[i + 1].y
+                );
+        ctx.stroke();
+    };
+
     var onPaint = function () {
 
         if (tool == 'brush')
@@ -208,6 +252,10 @@
         else if (tool == 'ellipse')
         {
             onPaintEllipse();
+        }
+        else if (tool == 'eraser')
+        {
+            onErase();
         }
 
     };
