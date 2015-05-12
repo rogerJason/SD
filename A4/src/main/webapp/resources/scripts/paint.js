96020117854829d33e149065a52b88b60f583a9b
cipribudusan <cipribudusan@yahoo.com>
2015-05-12 14:30:02 +0300

    [A4] Drawing Rectangle with Fabric JS

--- A4/src/main/webapp/resources/scripts/paint.js
+++ A4/src/main/webapp/resources/scripts/paint.js
@@ -1,3 +1,5 @@
+/* global fabric */
+
 $.getScript("resources/scripts/fabric.min.js", function () {
 
     // Obtain a canvas drawing surface from fabric.js
@@ -8,48 +10,20 @@ $.getScript("resources/scripts/fabric.min.js", function () {
     var sketch_style = getComputedStyle(sketch);
     canvas.setWidth(parseInt(sketch_style.getPropertyValue('width')));
     canvas.setHeight(parseInt(sketch_style.getPropertyValue('height')));
-    
-    var drawingModeEl = $('#mode button');
-
-    // ---------------------------------- SELECTING THE MODE FOR DRAWING ---------------------------------
-    // current mode
-    var mode = 'draw';
-    canvas.isDrawingMode = true;
-
-    drawingModeEl.on('click', function () {
-        mode = $(this).attr('id');
-        console.log(mode);  // it is used for debugging, press F12 in Chrome and select Console
-        if (mode === 'draw')
-        {
-            canvas.isDrawingMode = true;
-        }
-        else if (mode === 'select')
-        {
-            canvas.isDrawingMode = false;
-        }
-    });
-    // -----------------------------------------------------------------------------------------------------------
 
     // ---------------------------------- SELECTING THE CURRENT TOOL FOR DRAWING ---------------------------------
     // current tool
-    var tool = 'brush';
+    var tool = 'eraser';
 
     $('#tools button').on('click', function () {
         tool = $(this).attr('id');
+        canvas.isDrawingMode = false;
+        canvas.selection = false;
+        canvas.off('mouse:down');
+        canvas.off('mouse:move');
+        canvas.off('mouse:up'); // reset the mouse events
+        drawFigure();   // each figure has its own set of mouse events
         console.log(tool);  // it is used for debugging, press F12 in Chrome and select Console
-    });
-    // -----------------------------------------------------------------------------------------------------------
-
-    // ---------------------------------- SELECTING THE CURRENT COLOR FOR DRAWING ---------------------------------
-
-    document.getElementById("drawing_color").addEventListener("change", function () {
-        canvas.freeDrawingBrush.color = document.getElementById("drawing_color").value;
-    });
-    // -----------------------------------------------------------------------------------------------------------
-
-    // ---------------------------------- SELECTING THE LINE WIDTH FOR DRAWING ---------------------------------
-    document.getElementById("width_range").addEventListener("change", function () {
-        canvas.freeDrawingBrush.width = parseInt(document.getElementById("width_range").value);
     });
     // -----------------------------------------------------------------------------------------------------------
 
@@ -61,24 +35,98 @@ $.getScript("resources/scripts/fabric.min.js", function () {
     });
     // -----------------------------------------------------------------------------------------------------------
 
-    var mouse = {x: 0, y: 0};
-    var start_mouse = {x: 0, y: 0}; // used for saving the starting coordinates for drawing the line
-    var last_mouse = {x: 0, y: 0};
+    var drawBrush = function () {
+        canvas.isDrawingMode = true;
+        // ---------------------------------- SELECTING THE CURRENT COLOR FOR DRAWING ---------------------------------
 
+        document.getElementById("drawing_color").addEventListener("change", function () {
+            canvas.freeDrawingBrush.color = document.getElementById("drawing_color").value;
+        });
+        // -----------------------------------------------------------------------------------------------------------
 
-    // Create a text object. 
-    // Does not display it-the canvas doesn't 
-    // know about it yet.
-    var hi = new fabric.Text('TYLER THE CREATOR', {
-        left: canvas.getWidth() / 4,
-        top: canvas.getHeight() / 4
-    });
+        // ---------------------------------- SELECTING THE LINE WIDTH FOR DRAWING ---------------------------------
+        document.getElementById("width_range").addEventListener("change", function () {
+            canvas.freeDrawingBrush.width = parseInt(document.getElementById("width_range").value);
+        });
+        // -----------------------------------------------------------------------------------------------------------
+    };
 
-    // Attach it to the canvas object, then (re)display
-    // the canvas.	
-    canvas.add(hi);
+    var drawRect = function () {
+        var rect, isDown, origX, origY;
 
-    canvas.renderAll();
-    canvas.calcOffset();    // recalculate all the objects positions
+        canvas.on('mouse:down', function (o) {
+            isDown = true;
+            var pointer = canvas.getPointer(o.e);
+            origX = pointer.x;
+            origY = pointer.y;
+            var pointer = canvas.getPointer(o.e);
+            rect = new fabric.Rect({
+                left: origX,
+                top: origY,
+                originX: 'left',
+                originY: 'top',
+                width: pointer.x - origX,
+                height: pointer.y - origY,
+                angle: 0,
+                fill: 'rgba(255,0,0,0.5)',
+                transparentCorners: false
+            });
+            canvas.add(rect);
+        });
+
+        canvas.on('mouse:move', function (o) {
+            if (!isDown)
+                return;
+            var pointer = canvas.getPointer(o.e);
+
+            if (origX > pointer.x) {
+                rect.set({left: Math.abs(pointer.x)});
+            }
+            if (origY > pointer.y) {
+                rect.set({top: Math.abs(pointer.y)});
+            }
+
+            rect.set({width: Math.abs(origX - pointer.x)});
+            rect.set({height: Math.abs(origY - pointer.y)});
+
+            canvas.renderAll();
+        });
+
+        canvas.on('mouse:up', function (o) {
+            isDown = false;
+        });
+    };
+
+    var drawFigure = function () {
+
+        if (tool === 'brush')
+        {
+            drawBrush();
+        }
+        else if (tool === 'circle')
+        {
+            onPaintCircle();
+        }
+        else if (tool === 'line')
+        {
+            onPaintLine();
+        }
+        else if (tool === 'rectangle')
+        {
+            drawRect();
+        }
+        else if (tool === 'ellipse')
+        {
+            drawEllipse(tmp_ctx);
+        }
+        else if (tool === 'eraser')
+        {
+            onErase();
+        }
+        else if (tool === 'spray')
+        {
+            generateSprayParticles();
+        }
+    };
 
 });
